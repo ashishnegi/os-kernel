a20db90fd63dcfb4830e3d333171bc400a0967ab
Ashish Negi <ashishnegi33@gmail.com>
2013-10-21 18:13:43 +0530

    made variables for directories and files

--- nasm_boot_sect.bat
+++ nasm_boot_sect.bat
@@ -1,3 +1,13 @@
-nasm boot_sect.asm -f bin -o "Qemu-1.5.3-windows\Qemu-windows-1.5.3\boot\boot_sect.asm"
+:: @echo off
+setlocal EnableDelayedExpansion
+set qemu_base=Qemu-1.5.3-windows\Qemu-windows-1.5.3
+set boot_base=boot
+set bios_base=Bios 
+set boot_sect=boot_sect.asm
+set qemu_system=qemu-system-i386.exe
 
-"Qemu-1.5.3-windows\Qemu-windows-1.5.3\qemu-system-i386" -L "Qemu-1.5.3-windows\Qemu-windows-1.5.3\Bios" "Qemu-1.5.3-windows\Qemu-windows-1.5.3\boot\boot_sect.asm"
+echo %qemu_base%\%boot_base%\%boot_sect%
+
+nasm boot_sect.asm -f bin -o %qemu_base%\%boot_base%\%boot_sect%
+
+%qemu_base%\%qemu_system% -L  %qemu_base%\%bios_base%    %qemu_base%\%boot_base%\%boot_sect%
