52f4f2459a30dcdbce89cc581fde17a9b967ef3f
Ashish Negi <ashishnegi33@gmail.com>
2013-10-21 22:40:54 +0530

    chapter 3: learning offset, memory, addresses, labels

---
+++ offset_boot_sect.asm
@@ -0,0 +1,22 @@
+
+the_secret:
+	db "X"
+
+mov ah, 0x0e
+
+mov al, the_secret
+int 10h
+
+mov al, [the_secret]
+int 10h
+
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 10h
+
+jmp $			; loop infinitely here
+
+times 510 - ($-$$) db 0
+
+dw 0xaa55
