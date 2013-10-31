8248b1e9ec830c61ebe8ed1733dc9605f93c768c
Ashish Negi <ashishnegi33@gmail.com>
2013-10-31 23:17:33 +0530

    edx should point to video_mem and not to what is stored in it

--- print_string_32.asm
+++ print_string_32.asm
@@ -1,13 +1,14 @@
+; always have [bits 32] before including this file
 [bits 32]
 
-VIDEO_MEM : 0xb8000
-WHITE_ON_BLACK : 0x0f
+VIDEO_MEM equ 0xb8000
+WHITE_ON_BLACK equ 0x0f
 
 ; How to switch to 32 bit ?
 
 print_string_32 :
 	pusha						; store all the registers
-	mov edx, [VIDEO_MEM]
+	mov edx, VIDEO_MEM
 
 	print_string_32_loop:
 	mov al, [ebx]
